1eb8a39f026c5f0eba323eb07483747857deaede
florian <bessonflorian42@yahoo.fr>
2025-08-30 13:08:22 +0200

    fsdfsd

--- src/index.php
+++ src/index.php
@@ -1,4 +1,77 @@
-<?php
-phpinfo();
+ <?php 
+/*     $userAge = 17; */
+/*     $name = "florian"; */
+/*     $i = 1; */
+/*     /1* echo "bonjour {$name} {$userAge} et bienvenue"; *1/ */
+/*     /1*     while ($i < 100){ *1/ */
+/*     /1*         echo "j'ai {$i} ans <br/>"; *1/ */
+/*     /1*         $i++; *1/ */
+/*     /1* } *1/ */
+/*     // dot = concatenation */
+/*     echo "----- NEW TEST ----- <br/>"; */
+/*     $pokemon = [ */
+/*         [ */
+/*             'name' => 'Pikachu', */
+/*             'type' => 'Electrique', */
+/*             'difficulty' => 'Easy', */
+/*         ], */
+/*         [ */
+/*             'name' => 'Charizard', */
+/*             'type' => 'Fire', */
+/*             'difficulty' => 'Medium', */
+/*         ], */
+/*         [ */
+/*             'name' => 'Kyogre', */
+/*             'type' => 'Water', */
+/*             'difficulty' => 'Hard', */
+/*         ], */
+/*     ]; */
+/*     $i = 0; */
+/*     while ($i < 3) { */
+/*         echo $pokemon[$i]['name'] . ' ' . $pokemon[$i]['type'] . '<br />'; */
+/*         $i++; */
+/*     } */
+
+/* $recipes = [ */
+/*     [ */
+/*         'title' => 'Cassoulet', */
+/*         'author' => 'mickael.andrieu@exemple.com', */
+/*     ], */
+/*     [ */
+/*         'title' => 'Couscous', */
+/*         'author' => 'mickael.andrieu@exemple.com', */
+/*     ], */
+/*     [ */
+/*         'title' => 'Escalope milanaise', */
+/*         'author' => 'mathieu.nebra@exemple.com', */
+/*     ], */
+/*     [ */
+/*         'title' => 'Salade Romaine', */
+/*         'author' => 'laurene.castor@exemple.com', */
+/*     ], */
+/* ]; */
+/* ?> */
+
+/* <html> */
+/* <head> */
+/*     <meta charset="UTF-8"> */
+/*     <meta http-equiv="X-UA-Compatible" content="IE=edge"> */
+/*     <meta name="viewport" content="width=device-width, initial-scale=1.0"> */
+/*     <title>Affichage des recettes</title> */
+/* </head> */
+/* <body> */
+/*     <ul> */
+/*         <?php for ($lines = 0; $lines <= 3; $lines++) : ?> */
+/*             <li><?php echo $recipes[$lines]['title'] . ' (' . $recipes[$lines]['author'] . ')'; ?></li> */
+/*         <?php endfor; ?> */
+/*     </ul> */
+/* </body> */
+/* </html> */
+
+ /* SKIP la partie d'avant c'est que des trucs simple. lien avec tt les fonctions php si besoin */
+/* https://www.php.net/manual/fr/funcref.php */
 
 
+echo "----web request----"
+
+?>
