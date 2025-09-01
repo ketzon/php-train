35212f831cbf999de1a7c6bcfe8af941a2ef3947
florian <bessonflorian42@yahoo.fr>
2025-09-01 17:47:20 +0200

    form php

--- src/index.php
+++ src/index.php
@@ -1,77 +1,37 @@
- <?php 
-/*     $userAge = 17; */
-/*     $name = "florian"; */
-/*     $i = 1; */
-/*     /1* echo "bonjour {$name} {$userAge} et bienvenue"; *1/ */
-/*     /1*     while ($i < 100){ *1/ */
-/*     /1*         echo "j'ai {$i} ans <br/>"; *1/ */
-/*     /1*         $i++; *1/ */
-/*     /1* } *1/ */
-/*     // dot = concatenation */
-/*     echo "----- NEW TEST ----- <br/>"; */
-/*     $pokemon = [ */
-/*         [ */
-/*             'name' => 'Pikachu', */
-/*             'type' => 'Electrique', */
-/*             'difficulty' => 'Easy', */
-/*         ], */
-/*         [ */
-/*             'name' => 'Charizard', */
-/*             'type' => 'Fire', */
-/*             'difficulty' => 'Medium', */
-/*         ], */
-/*         [ */
-/*             'name' => 'Kyogre', */
-/*             'type' => 'Water', */
-/*             'difficulty' => 'Hard', */
-/*         ], */
-/*     ]; */
-/*     $i = 0; */
-/*     while ($i < 3) { */
-/*         echo $pokemon[$i]['name'] . ' ' . $pokemon[$i]['type'] . '<br />'; */
-/*         $i++; */
-/*     } */
-
-/* $recipes = [ */
-/*     [ */
-/*         'title' => 'Cassoulet', */
-/*         'author' => 'mickael.andrieu@exemple.com', */
-/*     ], */
-/*     [ */
-/*         'title' => 'Couscous', */
-/*         'author' => 'mickael.andrieu@exemple.com', */
-/*     ], */
-/*     [ */
-/*         'title' => 'Escalope milanaise', */
-/*         'author' => 'mathieu.nebra@exemple.com', */
-/*     ], */
-/*     [ */
-/*         'title' => 'Salade Romaine', */
-/*         'author' => 'laurene.castor@exemple.com', */
-/*     ], */
-/* ]; */
-/* ?> */
-
-/* <html> */
-/* <head> */
-/*     <meta charset="UTF-8"> */
-/*     <meta http-equiv="X-UA-Compatible" content="IE=edge"> */
-/*     <meta name="viewport" content="width=device-width, initial-scale=1.0"> */
-/*     <title>Affichage des recettes</title> */
-/* </head> */
-/* <body> */
-/*     <ul> */
-/*         <?php for ($lines = 0; $lines <= 3; $lines++) : ?> */
-/*             <li><?php echo $recipes[$lines]['title'] . ' (' . $recipes[$lines]['author'] . ')'; ?></li> */
-/*         <?php endfor; ?> */
-/*     </ul> */
-/* </body> */
-/* </html> */
-
- /* SKIP la partie d'avant c'est que des trucs simple. lien avec tt les fonctions php si besoin */
-/* https://www.php.net/manual/fr/funcref.php */
-
-
-echo "----web request----"
-
+<!-- inclusion des variables et fonctions -->
+<?php
+require_once(__DIR__ . '/variables.php');
+require_once(__DIR__ . '/functions.php');
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Page d'accueil</title>
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+        <!-- inclusion de l'entête du site -->
+        <?php require_once(__DIR__ . '/header.php'); ?>
+        <h1>Site de recettes</h1>
+
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+            </article>
+        <?php endforeach ?>
+    </div>
+
+    <!-- inclusion du bas de page du site -->
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
